b754cbfc26350eb8970b8b74570b09633cd9ce98
Asher Lien <birdym.tw@gmail.com>
2025-03-22 16:44:40 +0800

    第一次建立檔案

---
+++ app/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { appName } from '~/constants'
+
+useHead({
+  title: appName,
+})
+</script>
+
+<template>
+  <VitePwaManifest />
+  <NuxtLayout>
+    <UApp>
+      <NuxtPage />
+    </UApp>
+  </NuxtLayout>
+</template>
+
+<style>
+html,
+body,
+#__nuxt {
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+}
+
+html.dark {
+  color-scheme: dark;
+}
+</style>
---
+++ app/pages/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'home',
+})
+</script>
+
+<template>
+  <div>
+    <UButton>
+      Click me
+    </UButton>
+    <Logos class="h-10 w-10" />
+  </div>
+</template>
