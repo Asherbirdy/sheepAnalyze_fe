c8c5d231fee514a7475fa3936fe7bd2eb0fbbaa5
Asher Lien <birdym.tw@gmail.com>
2025-03-31 10:43:11 +0800

    basicui

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,7 +1,124 @@
-<script setup lang='ts'>
+<script setup lang="ts">
+const state = reactive({
+  name: 'Benjamin Canac',
+  username: 'benjamincanac',
+  currentPassword: '',
+  newPassword: '',
+  confirmPassword: '',
+})
 
+const items = [
+  {
+    label: 'Login',
+    description: 'Make changes to your account here. Click save when you\'re done.',
+    icon: 'i-lucide-user',
+    slot: 'login' as const,
+  },
+  {
+    label: 'Register',
+    description: 'Change your password here. After saving, you\'ll be logged out.',
+    icon: 'i-lucide-lock',
+    slot: 'register' as const,
+  },
+]
 </script>
 
 <template>
-  <div>login</div>
+  <UTabs
+    :items="items"
+    class="gap-4 w-full"
+    :ui="{ trigger: 'flex-1' }"
+  >
+    <template #login="{ item }">
+      <p class="text-(--ui-text-muted) mb-4">
+        {{ item.description }}
+      </p>
+
+      <UForm
+        :state="state"
+        class="flex flex-col gap-4"
+      >
+        <UFormField
+          label="Name"
+          name="name"
+        >
+          <UInput
+            v-model="state.name"
+            class="w-full"
+          />
+        </UFormField>
+        <UFormField
+          label="Username"
+          name="username"
+        >
+          <UInput
+            v-model="state.username"
+            class="w-full"
+          />
+        </UFormField>
+
+        <UButton
+          label="Save changes"
+          type="submit"
+          variant="soft"
+          class="self-end"
+        />
+      </UForm>
+    </template>
+
+    <template #register="{ item }">
+      <p class="text-(--ui-text-muted) mb-4">
+        {{ item.description }}
+      </p>
+
+      <UForm
+        :state="state"
+        class="flex flex-col gap-4"
+      >
+        <UFormField
+          label="Current Password"
+          name="current"
+          required
+        >
+          <UInput
+            v-model="state.currentPassword"
+            type="password"
+            required
+            class="w-full"
+          />
+        </UFormField>
+        <UFormField
+          label="New Password"
+          name="new"
+          required
+        >
+          <UInput
+            v-model="state.newPassword"
+            type="password"
+            required
+            class="w-full"
+          />
+        </UFormField>
+        <UFormField
+          label="Confirm Password"
+          name="confirm"
+          required
+        >
+          <UInput
+            v-model="state.confirmPassword"
+            type="password"
+            required
+            class="w-full"
+          />
+        </UFormField>
+
+        <UButton
+          label="Change password"
+          type="submit"
+          variant="soft"
+          class="self-end"
+        />
+      </UForm>
+    </template>
+  </UTabs>
 </template>
