2d6fac90fae5fbd79d610cb8fb8bbb54b6a99bce
Asher Lien <birdym.tw@gmail.com>
2025-03-31 11:49:16 +0800

    更新

--- app/apis/useAuthApi.ts
+++ app/apis/useAuthApi.ts
@@ -2,27 +2,30 @@ import type { LoginError, LoginPayload, LoginResponse } from '~/type'
 import { useRequestApi } from '~/composables/useRequestApi'
 
 export const useAuthApi = {
-  login: async (payload: LoginPayload) => await useRequestApi<LoginResponse, LoginError>('/auth/login', {
-    method: 'POST',
-    body: payload,
-    server: false,
-    lazy: true,
-    immediate: false,
-    watch: false,
-  }),
-  register: async (state: any) => await useRequestApi('/auth/userRegister', {
-    method: 'POST',
-    body: state,
-    server: false,
-    lazy: true,
-    immediate: false,
-    watch: false,
-  }),
+  login: async (payload: LoginPayload) =>
+    await useRequestApi<LoginResponse, LoginError>('/auth/login', {
+      method: 'POST',
+      body: payload,
+      server: false,
+      lazy: true,
+      immediate: false,
+      watch: false,
+    }),
+  register: async (state: any) =>
+    await useRequestApi('/auth/userRegister', {
+      method: 'POST',
+      body: state,
+      server: false,
+      lazy: true,
+      immediate: false,
+      watch: false,
+    }),
   // 存會員登入狀態
-  showMe: async () => await useRequestApi('/users/showMe', {
-    method: 'GET',
-    server: false,
-    lazy: true,
-    watch: false,
-  }),
+  showMe: async () =>
+    await useRequestApi('/users/showMe', {
+      method: 'GET',
+      server: false,
+      lazy: true,
+      watch: false,
+    }),
 }
