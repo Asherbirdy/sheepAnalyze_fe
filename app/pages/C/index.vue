445b39dfd6b25199a58697fdf240afba05efee6e
Asher Lien <birdym.tw@gmail.com>
2025-04-04 18:00:24 +0800

    定義api

--- app/pages/C/index.vue
+++ app/pages/C/index.vue
@@ -16,6 +16,7 @@ const {
       <h2>姓名：{{ UserInfoResponse?.user.name }}</h2>
       <h2>電子信箱：{{ UserInfoResponse?.user.email }}</h2>
       <h2>區域：{{ UserInfoResponse?.user.district.name }}</h2>
+      <h2>Email驗證：{{ UserInfoResponse?.user.emailVerified }}</h2>
       <h2>權限：{{ UserInfoResponse?.user.role }}</h2>
       <h2>網頁權限：{{ UserInfoResponse?.user.landingPageAccess }}</h2>
     </UCard>
--- app/type/index.ts
+++ app/type/index.ts
@@ -1,8 +1,11 @@
 // * API
+// useAuthApi
+export * from './api/useAuthApi/BindOTPEmailApiType'
 export * from './api/useAuthApi/CheckValidTokenApiType'
 export * from './api/useAuthApi/LoginApiType'
 export * from './api/useAuthApi/RegisterApiType'
 export * from './api/useUserApi/ShowMeApiType'
 
 // * APP
+
 // * COMMON
