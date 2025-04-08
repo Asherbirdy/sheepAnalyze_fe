a3c5ffec210535e10f6e9baeec5a4a7322c843d9
Asher Lien <birdym.tw@gmail.com>
2025-04-08 11:52:20 +0800

    wip

--- app/pages/C/landingPageEditor/[id].vue
+++ app/pages/C/landingPageEditor/[id].vue
@@ -1,9 +1,35 @@
 <script setup lang="ts">
-const route = useRoute('C-landingPageEditor-id')
+import StarterKit from '@tiptap/starter-kit'
+import { Editor, EditorContent } from '@tiptap/vue-3'
+
+const editor = ref()
+const editorHTML = ref('')
+
+const getHTML = () => {
+  editorHTML.value = editor?.value?.getHTML()
+}
+
+const init = () => {
+  editor.value = new Editor({
+    content: '<p>I\'m running Tiptap with Vue.js. 🎉</p>',
+    extensions: [StarterKit],
+  })
+}
+
+const destroy = () => {
+  editor.value.destroy()
+}
+
+onMounted(init)
+onBeforeUnmount(destroy)
 </script>
 
 <template>
   <div>
-    <h1>{{ route.params.id }}</h1>
+    <EditorContent :editor="editor" />
+    <button @click="getHTML">
+      getHTML
+    </button>
+    {{ editorHTML }}
   </div>
 </template>
