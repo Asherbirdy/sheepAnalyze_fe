755d9152f5cba8af05825cffb1360c004dfc14ae
Asher Lien <birdym.tw@gmail.com>
2025-04-14 17:59:56 +0800

    new

--- app/pages/C/blending.vue
+++ app/pages/C/blending.vue
@@ -1,10 +1,15 @@
 <script setup lang='ts'>
+import { useBlendingApi } from '~/apis'
 
+const { data: blendingData } = await useBlendingApi.get()
+
+const data = computed(() => blendingData.value?.response.filter(item => item.name !== ''))
 </script>
 
 <template>
   <div>
     <h1>2025blem</h1>
+    <pre>{{ data }}</pre>
   </div>
 </template>
 
