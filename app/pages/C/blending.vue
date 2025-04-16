f9be7598f145d6430f32d101421e23542f584afd
Asher Lien <birdym.tw@gmail.com>
2025-04-16 17:50:57 +0800

    wip

--- app/pages/C/blending.vue
+++ app/pages/C/blending.vue
@@ -1,14 +1,27 @@
 <script setup lang='ts'>
 import { useBlendingApi } from '~/apis'
 
+enum AgeRange {
+  Elder = '年長',
+}
+
 const { data: blendingData } = await useBlendingApi.get()
 
 const data = computed(() => blendingData.value?.response.filter(item => item.name !== ''))
+
+const elderDataList = computed(() => {
+  return data.value
+    ?.filter(item => item.ageRange === AgeRange.Elder)
+    .map(item => item.name) || []
+})
 </script>
 
 <template>
   <div>
     <h1>2025blem</h1>
+    <p>報名人數{{ data?.length }}</p>
+    <p>長者報名{{ elderDataList?.length }}</p>
+    <p>{{ elderDataList }}</p>
     <pre>{{ data }}</pre>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,7 +18,7 @@ export default defineNuxtConfig({
 
   app: {
     head: {
-      viewport: 'width=device-width,initial-scale=1',
+      viewport: 'width=device-width,initial-scale=1,user-scalable=no',
       link: [
         { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
         { rel: 'icon', type: 'image/svg+xml', href: '/nuxt.svg' },
