6bcf940a57442f07e2e4ea82e9ce8243305018e2
Asher Lien <birdym.tw@gmail.com>
2025-05-05 11:07:04 +0800

    basic ui

--- app/pages/C/sheet.vue
+++ app/pages/C/sheet.vue
@@ -7,14 +7,31 @@ const { data: sheets } = await useSheetApi.getAll()
 <template>
   <div>
     <h1>Sheet</h1>
-
     <UCard
       v-for="sheet in sheets?.sheets"
       :key="sheet._id"
     >
-      <p>{{ sheet._id }}</p>
-      <p>{{ sheet.name }}</p>
-      <p>{{ sheet.api }}</p>
+      <div class="flex items-center justify-between">
+        <div>
+          <p>id: {{ sheet._id }}</p>
+          <p>name: {{ sheet.name }}</p>
+          <p>api: {{ sheet.api }}</p>
+        </div>
+        <div class="flex flex-col gap-2">
+          <UButton
+            icon="i-lucide-pencil"
+            size="xs"
+            color="primary"
+            variant="solid"
+          />
+          <UButton
+            icon="i-lucide-trash"
+            size="xs"
+            color="primary"
+            variant="solid"
+          />
+        </div>
+      </div>
     </UCard>
   </div>
 </template>
