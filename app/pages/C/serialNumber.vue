c52001d23df63ef8ca30bf51fbd525519dcfe1d3
Asher Lien <birdym.tw@gmail.com>
2025-05-19 11:26:55 +0800

    更新

--- app/pages/C/serialNumber.vue
+++ app/pages/C/serialNumber.vue
@@ -6,18 +6,36 @@ const { data: SerialNumbersResponse } = await useSerialNumberApi.getAll()
 
 <template>
   <div>
-    <UCard>
+    <div class="flex items-center gap-2 justify-between">
+      <p class="text-md font-bold mb-4">
+        序號列表
+      </p>
+      <UButton>
+        創建序號
+      </UButton>
+    </div>
+    <UCard
+      v-for="serialNumber in SerialNumbersResponse?.userSerialNumber"
+      :key="serialNumber._id"
+      class="mb-4"
+    >
       <template #header>
-        <Placeholder class="h-8" />
+        <div class="flex items-center gap-2 justify-between">
+          <div class="flex items-center gap-2">
+            <UBadge :color="serialNumber.isUsed ? 'success' : 'warning'">
+              {{ serialNumber.isUsed ? '已使用' : '未使用' }}
+            </UBadge>
+            <p>{{ serialNumber.notes || '無備註' }}</p>
+          </div>
+          <UIcon name="i-lucide-trash" />
+        </div>
       </template>
-
-      <Placeholder class="h-32" />
-
-      <template #footer>
-        <Placeholder class="h-8" />
-      </template>
+      <p>
+        序號：{{ serialNumber.serialNumber }}
+      </p>
+      <p>角色：{{ serialNumber.role }}</p>
+      <p>區：{{ serialNumber.districtId.name }}</p>
+      <p>創建時間：{{ serialNumber.createdAt }}</p>
     </UCard>
-
-    {{ SerialNumbersResponse }}
   </div>
 </template>
