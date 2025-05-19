9757add950ceb57b28806504ff029ab6ee042ba6
Asher Lien <birdym.tw@gmail.com>
2025-05-19 11:52:49 +0800

    DeleteSerialNumberComponent

--- app/components/page/C/SerialNumber/DeleteSerialNumberComponent.vue
+++ app/components/page/C/SerialNumber/DeleteSerialNumberComponent.vue
@@ -1,11 +1,58 @@
 <script setup lang='ts'>
+import type { StateType } from '~/type'
 
+interface FeatureType {
+  modal: {
+    open: boolean
+  }
+}
+
+interface PropsType {
+  serialNumberId: string
+}
+
+const props = defineProps<PropsType>()
+const { serialNumberId } = toRefs(props)
+
+const state = ref<StateType<null, FeatureType>>({
+  feature: {
+    modal: {
+      open: false,
+    },
+  },
+  data: null,
+})
 </script>
 
 <template>
-  <UIcon name="i-lucide-trash" />
+  <div>
+    <UIcon
+      name="i-lucide-trash"
+      class="size-5"
+      @click="state.feature.modal.open = true"
+    />
+  </div>
+  <UModal
+    v-model:open="state.feature.modal.open"
+    title="是否刪除序號"
+    :ui="{ footer: 'justify-end' }"
+  >
+    <template #body>
+      <p>
+        {{ `是否刪除序號 ${serialNumberId}` }}
+      </p>
+    </template>
+    <template #footer>
+      <UButton
+        label="取消"
+        color="neutral"
+        variant="outline"
+        @click="state.feature.modal.open = false"
+      />
+      <UButton
+        label="確認"
+        variant="outline"
+      />
+    </template>
+  </UModal>
 </template>
-
-<style>
-
-</style>
--- app/pages/C/serialNumber.vue
+++ app/pages/C/serialNumber.vue
@@ -7,11 +7,12 @@ const { data: SerialNumbersResponse } = await useSerialNumberApi.getAll()
 
 <template>
   <div>
-    <div class="flex items-center gap-2 justify-between">
-      <p class="text-md font-bold mb-4">
+    <div class="flex items-center gap-2 justify-between mb-4">
+      <p class="text-md font-bold">
         序號列表
       </p>
       <UButton>
+        <UIcon name="i-lucide-plus" />
         創建序號
       </UButton>
     </div>
@@ -23,17 +24,18 @@ const { data: SerialNumbersResponse } = await useSerialNumberApi.getAll()
       <template #header>
         <div class="flex items-center gap-2 justify-between">
           <div class="flex items-center gap-2">
-            <UBadge :color="serialNumber.isUsed ? 'success' : 'warning'">
+            <UBadge
+              :color="serialNumber.isUsed ? 'success' : 'warning'"
+              variant="outline"
+            >
               {{ serialNumber.isUsed ? '已使用' : '未使用' }}
             </UBadge>
             <p>{{ serialNumber.notes || '無備註' }}</p>
           </div>
-          <DeleteSerialNumberComponent />
+          <DeleteSerialNumberComponent :serial-number-id="serialNumber._id" />
         </div>
       </template>
-      <p>
-        序號：{{ serialNumber.serialNumber }}
-      </p>
+      <p>序號：{{ serialNumber.serialNumber }}</p>
       <p>角色：{{ serialNumber.role }}</p>
       <p>區：{{ serialNumber.districtId.name }}</p>
       <p>創建時間：{{ serialNumber.createdAt }}</p>
