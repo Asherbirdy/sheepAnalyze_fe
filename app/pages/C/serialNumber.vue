6e1d85a571c8684a0dd091327155ff0112c0a6ae
Asher Lien <birdym.tw@gmail.com>
2025-05-19 10:48:33 +0800

    更新

--- app/pages/C/serialNumber.vue
+++ app/pages/C/serialNumber.vue
@@ -1,5 +1,23 @@
+<script setup lang="ts">
+import { useSerialNumberApi } from '~/apis'
+
+const { data: SerialNumbersResponse } = await useSerialNumberApi.getAll()
+</script>
+
 <template>
   <div>
-    <h1>Serial Number</h1>
+    <UCard>
+      <template #header>
+        <Placeholder class="h-8" />
+      </template>
+
+      <Placeholder class="h-32" />
+
+      <template #footer>
+        <Placeholder class="h-8" />
+      </template>
+    </UCard>
+
+    {{ SerialNumbersResponse }}
   </div>
 </template>
