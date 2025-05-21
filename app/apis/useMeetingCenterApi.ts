2678c8a620b3495f1603029e9f6f029809e76c58
Asher Lien <birdym.tw@gmail.com>
2025-05-21 15:24:57 +0800

    basix

--- app/apis/useMeetingCenterApi.ts
+++ app/apis/useMeetingCenterApi.ts
@@ -1,13 +1,29 @@
 import { useRequestApi } from '~/composables'
 import { UserRequestUrl } from '~/enum'
 
+export interface MeetingCenterResponse {
+  msg: string
+  data: MeetingCenter[]
+}
+
+export interface MeetingCenter {
+  _id: string
+  districtName: string
+  name: string
+  identity: string
+  departure: string
+  returnRide: string
+  notes: string
+  createdAt: string
+  updatedAt: string
+  __v: number
+}
+
 export const useMeetingCenterApi = {
   getAll: async () =>
-    await useRequestApi(UserRequestUrl.MeetingCenter, {
+    await useRequestApi<MeetingCenterResponse, null>(UserRequestUrl.MeetingCenter, {
       method: 'GET',
       server: false,
-      lazy: true,
-      immediate: false,
     }),
   create: async () =>
     await useRequestApi(UserRequestUrl.MeetingCenter, {
--- app/pages/C/meetingCenter.vue
+++ app/pages/C/meetingCenter.vue
@@ -1,11 +1,105 @@
 <script setup lang='ts'>
+import { useMeetingCenterApi } from '~/apis'
 
+const {
+  data: MeetingCenterResponse,
+  refresh: MeetingCenterRequest,
+} = await useMeetingCenterApi.getAll()
+
+const {
+  execute: MeetingCenterCreateRequest,
+} = await useMeetingCenterApi.create()
+
+const data = computed(() => MeetingCenterResponse.value?.data)
+
+const districtOne = computed(() => data.value?.filter(
+  item => item.districtName === '一區',
+))
+const districtTwo = computed(() => data.value?.filter(
+  item => item.districtName === '二區',
+))
+const districtThree = computed(() => data.value?.filter(
+  item => item.districtName === '三區',
+))
+const districtFour = computed(() => data.value?.filter(
+  item => item.districtName === '四區',
+))
+
+const tabs = [
+  { label: '報名名單', slot: 'join' },
+  { label: '去程-遊覽車', slot: 'departure' },
+  { label: '回程-遊覽車', slot: 'return' },
+]
+
+const updateData = async () => {
+  await MeetingCenterCreateRequest()
+  await MeetingCenterRequest()
+}
 </script>
 
 <template>
-  <div>adsa</div>
+  <div>
+    <UButton @click="updateData">
+      <UIcon name="i-lucide-plus" />
+      更新數據
+    </UButton>
+    <UTabs
+      :items="tabs"
+      variant="link"
+      class="gap-4 w-full"
+      :ui="{ trigger: 'flex-1' }"
+    >
+      <template #join>
+        <div>總報名：{{ data?.length }}位</div>
+        <p>一區</p>
+        <div class="flex flex-wrap gap-2">
+          <UBadge
+            v-for="(item, index) in districtOne"
+            :key="index"
+            color="info"
+            variant="soft"
+            size="sm"
+          >
+            {{ item.name }}
+          </UBadge>
+        </div>
+        <p>二區</p>
+        <div class="flex flex-wrap gap-2">
+          <UBadge
+            v-for="(item, index) in districtTwo"
+            :key="index"
+            color="info"
+            variant="soft"
+            size="sm"
+          >
+            {{ item.name }}
+          </UBadge>
+        </div>
+        <p>三區</p>
+        <div class="flex flex-wrap gap-2">
+          <UBadge
+            v-for="(item, index) in districtThree"
+            :key="index"
+            color="info"
+            variant="soft"
+            size="sm"
+          >
+            {{ item.name }}
+          </UBadge>
+        </div>
+        <p>四區</p>
+        <div class="flex flex-wrap gap-2">
+          <UBadge
+            v-for="(item, index) in districtFour"
+            :key="index"
+            color="info"
+            variant="soft"
+            size="sm"
+          >
+            {{ item.name }}
+          </UBadge>
+        </div>
+      </template>
+    </UTabs>
+  </div>
 </template>
-
-<style>
-
-</style>
