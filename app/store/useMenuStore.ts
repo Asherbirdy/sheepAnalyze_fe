8e617c16e9f040d3139ab0ebb42bd4414572571a
Asher Lien <birdym.tw@gmail.com>
2025-05-26 09:56:06 +0800

    donne

--- app/store/useMenuStore.ts
+++ app/store/useMenuStore.ts
@@ -5,40 +5,45 @@ export function useMenuStore() {
 
   const menu = useState('menu', () => [
     {
-      label: 'Home',
-      icon: 'i-lucide-home',
+      label: '個人資料',
+      icon: 'i-lucide-user',
       to: ClientRoutes.Home,
       active: computed(() => route.path === ClientRoutes.Home),
     },
     {
-      label: 'Serial Number',
+      label: '帳號管理',
+      icon: 'mingcute:group-3-line',
+      active: computed(() => route.path === ClientRoutes.SerialNumber),
+    },
+    {
+      label: '帳號序號申請',
       icon: 'i-lucide-barcode',
       to: ClientRoutes.SerialNumber,
       active: computed(() => route.path === ClientRoutes.SerialNumber),
     },
     {
-      label: 'Landing Page',
+      label: '首頁編輯器',
       icon: 'fluent-mdl2:page',
       to: ClientRoutes.LandingPage,
       active: computed(() => route.path.startsWith(ClientRoutes.LandingPage)),
     },
     {
-      label: 'Blending',
-      icon: 'material-symbols-light:blender',
-      to: ClientRoutes.Blending,
-      active: computed(() => route.path === ClientRoutes.Blending),
-    },
-    // {
-    //   label: 'Sheet',
-    //   icon: 'i-lucide-file-spreadsheet',
-    //   to: ClientRoutes.Sheet,
-    //   active: computed(() => route.path === ClientRoutes.Sheet),
-    // },
-    {
-      label: '港湖集中主日',
-      icon: 'i-lucide-video',
-      to: ClientRoutes.MeetingCenter,
-      active: computed(() => route.path === ClientRoutes.MeetingCenter),
+      label: '表單分析',
+      icon: 'i-lucide-file-spreadsheet',
+      children: [
+        {
+          label: '相調',
+          icon: 'material-symbols-light:blender',
+          to: ClientRoutes.Blending,
+          active: computed(() => route.path === ClientRoutes.Blending),
+        },
+        {
+          label: '港湖集中主日',
+          icon: 'i-lucide-video',
+          to: ClientRoutes.MeetingCenter,
+          active: computed(() => route.path === ClientRoutes.MeetingCenter),
+        },
+      ],
     },
   ])
 
