cc4ec7718fa9bde0a83fa1642ec2521d7e416064
Asher Lien <birdym.tw@gmail.com>
2025-05-26 15:01:00 +0800

    更新

---
+++ app/middleware/auth.ts
@@ -0,0 +1,17 @@
+import { useUserApi } from '~/apis'
+import { PublicRoutes } from '~/enum'
+import { useUserStore } from '~/store/useUserStore'
+
+export default defineNuxtRouteMiddleware(async () => {
+  const { setUserInfo } = useUserStore()
+  const { data, error } = await useUserApi.showMe()
+
+  if (error.value) {
+    clearNuxtData() // 清掉 cache
+    return navigateTo(PublicRoutes.Login)
+  }
+
+  if (data.value && !error.value) {
+    setUserInfo(data.value.user)
+  }
+})
