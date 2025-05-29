39a0cf466f20dda359ea3b1651b00afb6e5d94fc
Asher Lien <birdym.tw@gmail.com>
2025-05-29 11:05:02 +0800

    ui

--- app/pages/lineoa/register.vue
+++ app/pages/lineoa/register.vue
@@ -10,9 +10,28 @@ const { LineProfile } = useLiff({
 </script>
 
 <template>
-  <div>
-    <pre>
-      LineProfile: {{ LineProfile }}
-    </pre>
+  <div class="flex flex-col items-center justify-center h-screen p-6 rounded-lg shadow-lg">
+    <h2 class="text-2xl font-extrabol mb-4">
+      點名者帳號綁定
+    </h2>
+    <img
+      :src="LineProfile?.pictureUrl"
+      alt="Line Profile"
+      class="w-24 h-24 rounded-full border-4 border-white shadow-md"
+    >
+    <p class="text-lg mt-2">
+      {{ LineProfile?.displayName }}
+    </p>
+
+    <UInput
+      label="序號"
+      :ui="{ root: 'w-full bg-white rounded-md shadow-sm' }"
+      class="mt-4"
+    />
+
+    <UButton
+      label="註冊"
+      block
+    />
   </div>
 </template>
