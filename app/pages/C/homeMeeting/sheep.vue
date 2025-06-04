8c34b154ca488899bea577720bb86cd858da44a4
Asher Lien <birdym.tw@gmail.com>
2025-06-04 16:54:02 +0800

    更新

--- app/pages/C/homeMeeting/sheep.vue
+++ app/pages/C/homeMeeting/sheep.vue
@@ -6,6 +6,22 @@ const { data: SheepListResponse } = await useSheepApi.list()
 
 <template>
   <div>
-    <pre>{{ SheepListResponse }}</pre>
+    <div class="flex items-center justify-between">
+      <h2>家聚會人位</h2>
+      <UButton size="sm">
+        <template #leading>
+          <UIcon name="i-heroicons-plus" />
+        </template>
+        新增
+      </UButton>
+    </div>
+    <UBadge
+      v-for="item in SheepListResponse?.sheep"
+      :key="item._id"
+      :label="item.name"
+      variant="soft"
+      color="primary"
+      class="m-1"
+    />
   </div>
 </template>
