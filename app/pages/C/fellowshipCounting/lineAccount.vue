b4c403ff3a3d25ae90b440d9bd1ef75d9ad4c3f7
Asher Lien <birdym.tw@gmail.com>
2025-06-18 16:36:06 +0800

    更新

--- app/pages/C/fellowshipCounting/lineAccount.vue
+++ app/pages/C/fellowshipCounting/lineAccount.vue
@@ -1,12 +1,20 @@
 <script setup lang="ts">
 import { useLineAccountMemberApi } from '~/apis'
 
-const { data } = await useLineAccountMemberApi.getAll()
+const { data: LineAccountResponse } = await useLineAccountMemberApi.getAll()
 </script>
 
 <template>
   <div>
-    <h1>Fellowship Counting</h1>
-    <p>{{ data }}</p>
+    <h1>Line帳號管理</h1>
+    <UBadge
+      v-for="item in LineAccountResponse?.data"
+      :key="item._id"
+      class="m-1"
+      :color="item.active ? 'primary' : 'warning'"
+      variant="soft"
+    >
+      {{ item.name }}{{ item.active ? '' : '(未審核)' }}
+    </UBadge>
   </div>
 </template>
